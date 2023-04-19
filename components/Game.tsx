fc08c004ccc63c949c24f911a5be53c263e79e98
spatulatom <spatulatom@gmail.com>
2023-04-19 13:04:25 +0100

    api

--- components/Game.tsx
+++ components/Game.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
-import { useEffect } from 'react'
-
+import React from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Game() {
+  const [solution, setSolution] = useState('');
+  const [guess, setGuesses] = useState(Array(6).fill(null));
+
+  const fetchWord = async () => {
+    const response = await fetch('api/hello');
+    const words = await response.json();
+
+    // Math.random gives us number 0 to 1, multipy by how many word there are, and floor that
+    // 2.5 would be 2
+    const randomWord =
+      words.words[Math.floor(Math.random() * words.words.length)];
+    console.log('kjkj', randomWord);
+    setSolution(randomWord);
+  };
+
+  useEffect(() => {
+    fetchWord();
+  }, []);
+
   return (
-    <div>Game</div>
-  )
+    <>
+      <div>Game </div>
+      <p className="text-white">{solution}</p>
+    </>
+  );
 }
