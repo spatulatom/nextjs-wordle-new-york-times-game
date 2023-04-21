5c84d49f16300bd0f4d1364ccd357abb83f9697d
spatulatom <spatulatom@gmail.com>
2023-04-21 11:41:19 +0100

    grid

--- components/Game.tsx
+++ components/Game.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from 'react';
 export default function Game() {
   const [solution, setSolution] = useState('');
   const [guesses, setGuesses] = useState(Array(6).fill(null));
-  
 
   const fetchWord = async () => {
     const response = await fetch('api/hello');
@@ -14,43 +13,50 @@ export default function Game() {
     // 2.5 would be 2
     const randomWord =
       words.words[Math.floor(Math.random() * words.words.length)];
-    setSolution(randomWord)
-  
+    setSolution(randomWord);
   };
 
   useEffect(() => {
     fetchWord();
   }, []);
-const lklk = ()=>{
-    fetchWord()
-   
-}
+  const lklk = () => {
+    fetchWord();
+  };
   return (
     <>
-      <div>Game </div>
-      <p className="text-white" onClick={lklk}>Click</p>
-      <h2>{solution}</h2>
-      {guesses.map(guess=>{
-        return(
-            <Line guess={guess?? ''}/>
-        )
+    
+      <p className="text-white" onClick={lklk}>
+        Click
+      </p>
+      <h2 className='mb-4'>{solution}</h2>
+      {guesses.map((guess) => {
+        return (
+          // https://sebhastian.com/javascript-double-question-mark/?utm_content=cmp-truehttps://sebhastian.com/javascript-double-question-mark/?utm_content=cmp-true
+          // Nullish Coalescing Operator
+          <Line guess={guess ?? ''} />
+        );
       })}
     </>
   );
 }
-type props ={
-    guess: any
-}
+type props = {
+  guess: any;
+};
 
-function Line({guess}:props) {
-    const WORD_LENGTH = 5;
-    const tiles = []
-for(let i =0; i < WORD_LENGTH; i++){
- const char = guess[i];
+function Line({ guess }: props) {
+  const WORD_LENGTH = 5;
+  const tiles = [];
 
-}
-
-  return (
-    <div>Game</div>
-  )
+  // if guess is null it will be turned into empty string '' so
+  // we need rather for loop then map() here
+  for (let i = 0; i < WORD_LENGTH; i++) {
+    const char = guess[i];
+    tiles.push(
+      <div key={i} className="w-16 h-16 border">
+        {char}
+      </div>
+    );
+  }
+  console.log('tiles', tiles);
+  return <div className="flex gap-2 mb-3">{tiles}</div>;
 }
