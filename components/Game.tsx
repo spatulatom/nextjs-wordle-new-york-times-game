479c78b9e78ce63c6219e2c52d43256805a91e5d
spatulatom <spatulatom@gmail.com>
2023-04-22 11:37:12 +0100

    more on keyboard events

--- components/Game.tsx
+++ components/Game.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 
 export default function Game() {
-  const [solution, setSolution] = useState('');
+  const [solution, setSolution] = useState('hello');
   //   why nulls not empty strings in the first place
   const [guesses, setGuesses] = useState(Array(6).fill(null));
   const [currentGuess, setCurrentGuess] = useState('');
@@ -18,6 +18,12 @@ export default function Game() {
         if (currentGuess.length !== 5) {
           return;
         }
+        const newGuesses = [...guesses];
+        newGuesses[guesses.findIndex((element) => element == null)] = currentGuess;
+        setGuesses(newGuesses)
+        setCurrentGuess('')
+  
+
         const isCorrect = solution === currentGuess;
         if (isCorrect) {
           setGameOver(true);
@@ -31,7 +37,6 @@ export default function Game() {
       if (currentGuess.length >= 5) {
         return;
       }
-     
 
       setCurrentGuess((oldGuess) => oldGuess + event.key);
     };
@@ -49,7 +54,7 @@ export default function Game() {
     // 2.5 would be 2
     const randomWord =
       words.words[Math.floor(Math.random() * words.words.length)];
-    setSolution(randomWord);
+    // setSolution(randomWord);
   };
 
   useEffect(() => {
@@ -72,7 +77,12 @@ export default function Game() {
           // https://sebhastian.com/javascript-double-question-mark/?utm_content=cmp-truehttps://sebhastian.com/javascript-double-question-mark/?utm_content=cmp-true
           // Nullish Coalescing Operator
           // if guess is null it will be turned into empty string '' on props: guess?? ''
-          <Line guess={isCurrentGuess ? currentGuess : guess ?? ''} />
+          <Line
+            guess={isCurrentGuess ? currentGuess : guess ?? ''}
+              isFinal={!isCurrentGuess && guess != null}
+            
+            solution={solution}
+          />
         );
       })}
     </>
@@ -80,9 +90,11 @@ export default function Game() {
 }
 type props = {
   guess: any;
+  isFinal: any;
+  solution: string;
 };
 
-function Line({ guess }: props) {
+function Line({ guess, isFinal, solution }: props) {
   const WORD_LENGTH = 5;
   //   this will be array of 5 divs and since its array of div no need to map over
   // it - remeber map() accutally returns an array of JSX elements
@@ -93,10 +105,24 @@ function Line({ guess }: props) {
   for (let i = 0; i < WORD_LENGTH; i++) {
     const char = guess[i];
     // console.log('hrtr', char) // when char ='' this code will evalute to undefinded
+    console.log('soltution', solution, char);
+    let tile = 'bg-black';
+    if (isFinal) {
+      if (char === solution[i]) {
+        console.log('here 1')
+        tile = 'bg-green-400';
+      } else if (solution.includes(char)) {
+        console.log('here 2')
+        tile = 'bg-yellow-500';
+      } else {
+        console.log('here 3')
+        tile = 'bg-gray-400';
+      }
+    }
     tiles.push(
       <div
         key={i}
-        className="w-16 h-16 border flex justify-center items-center uppercase text-xl"
+        className={`${tile} w-16 h-16 border flex justify-center items-center uppercase text-xl`}
       >
         {char}
       </div>
