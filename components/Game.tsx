a691480d77209453f1b79f48c982847cb2a74b3a
spatulatom <spatulatom@gmail.com>
2023-04-22 12:09:58 +0100

    guesses state

--- components/Game.tsx
+++ components/Game.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 
 export default function Game() {
-  const [solution, setSolution] = useState('hello');
+  const [solution, setSolution] = useState('');
   //   why nulls not empty strings in the first place
   const [guesses, setGuesses] = useState(Array(6).fill(null));
   const [currentGuess, setCurrentGuess] = useState('');
@@ -19,10 +19,10 @@ export default function Game() {
           return;
         }
         const newGuesses = [...guesses];
-        newGuesses[guesses.findIndex((element) => element == null)] = currentGuess;
-        setGuesses(newGuesses)
-        setCurrentGuess('')
-  
+        newGuesses[guesses.findIndex((element) => element == null)] =
+          currentGuess;
+        setGuesses(newGuesses);
+        setCurrentGuess('');
 
         const isCorrect = solution === currentGuess;
         if (isCorrect) {
@@ -38,8 +38,16 @@ export default function Game() {
         return;
       }
 
-      setCurrentGuess((oldGuess) => oldGuess + event.key);
+// to prevent other keys than letters
+const isLetter = event.key.match(/^[a-z]{1}$/) != null
+if(isLetter){
+
+      // the array elemments are uppercase so we need to change key events as well
+      // othwerwise styling comparison in Line component is not working a === A is false
+      setCurrentGuess((oldGuess) => oldGuess + event.key.toUpperCase());}
     };
+
+   
     window.addEventListener('keydown', handleType);
 
     // on onmount:
@@ -54,7 +62,7 @@ export default function Game() {
     // 2.5 would be 2
     const randomWord =
       words.words[Math.floor(Math.random() * words.words.length)];
-    // setSolution(randomWord);
+    setSolution(randomWord);
   };
 
   useEffect(() => {
@@ -79,8 +87,7 @@ export default function Game() {
           // if guess is null it will be turned into empty string '' on props: guess?? ''
           <Line
             guess={isCurrentGuess ? currentGuess : guess ?? ''}
-              isFinal={!isCurrentGuess && guess != null}
-            
+            isFinal={!isCurrentGuess && guess != null}
             solution={solution}
           />
         );
@@ -109,13 +116,13 @@ function Line({ guess, isFinal, solution }: props) {
     let tile = 'bg-black';
     if (isFinal) {
       if (char === solution[i]) {
-        console.log('here 1')
+        console.log('here 1');
         tile = 'bg-green-400';
       } else if (solution.includes(char)) {
-        console.log('here 2')
+        console.log('here 2');
         tile = 'bg-yellow-500';
       } else {
-        console.log('here 3')
+        console.log('here 3');
         tile = 'bg-gray-400';
       }
     }
