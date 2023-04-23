d4b8c14ad52ade810770e8f0e355f79a6a276316
spatulatom <spatulatom@gmail.com>
2023-04-23 11:16:49 +0100

    modal

--- components/Game.tsx
+++ components/Game.tsx
@@ -27,7 +27,7 @@ export default function Game() {
           return;
         }
         const newGuesses = [...guesses];
-        // The findIndex() method returns the index of THE FIRST element in an array that satisfies the provided testing function
+        // The findIndex() method returns the index of THEelement in an array that satisfies the provided testing function
         newGuesses[guesses.findIndex((element) => element == null)] =
           currentGuess;
         setGuesses(newGuesses);
--- components/Modal.tsx
+++ components/Modal.tsx
@@ -5,9 +5,9 @@ type props = {
 }
 export default function Modal({modalHandler}: props) {
   return (
-    <div onClick={modalHandler} className='fixed z-50 bg-black top-0 bottom-0 left-0 right-0 flex items-center justify-center'>
-    <div className='relative'>
-      <div className='absolute right-3 top-[-15px] border p-1' >CLOSE</div>
+    <div onClick={modalHandler} className='fixed z-50 bg-gray-500 bg-opacity-50 top-0 bottom-0 left-0 right-0 flex items-center justify-center'>
+    <div  onClick={(e) => e.stopPropagation()} className='relative z-51 bg-black'>
+      <div onClick={modalHandler} className='absolute right-3 top-1 border p-1' >CLOSE</div>
     <Image
     
     width={500}
