4d275a59b5ad9458e5cc3f8c81570dab5ae0f42e
spatulatom <spatulatom@gmail.com>
2023-04-23 16:24:22 +0100

    padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between py-36 bg-gray-900">
+    <main className="flex flex-col items-center justify-between py-36 px-2 bg-gray-900">
       <div className="z-10 w-full max-w-5xl flex flex-col items-center justify-evenly font-mono text-sm">
         <p className="fixed font-extrabold font-serif text-2xl left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-5 pt-5 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit  lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
          My  Wordle
