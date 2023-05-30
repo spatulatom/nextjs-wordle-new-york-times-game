1001a3a573d915cd7d7ee5da34ec20c69cdf0333
spatulatom <spatulatom@gmail.com>
2023-05-31 00:26:53 +0100

    prettier

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,21 +1,26 @@
-import React from 'react'
+import React from 'react';
 import Image from 'next/image';
 type props = {
-    modalHandler: ()=>void
-}
-export default function Modal({modalHandler}: props) {
+  modalHandler: () => void;
+};
+export default function Modal({ modalHandler }: props) {
   return (
-    <div onClick={modalHandler} className='fixed z-50 bg-gray-500 bg-opacity-50 top-0 bottom-0 left-0 right-0 flex items-center justify-center'>
-    <div  onClick={(e) => e.stopPropagation()} className='relative z-51 bg-black'>
-      <div onClick={modalHandler} className='absolute right-3 top-1 border p-1' >CLOSE</div>
-    <Image
-    
-    width={500}
-    height={500}
-    src="/ruless.png"
-    alt="How to play"/>
-    
+    <div
+      onClick={modalHandler}
+      className="fixed z-50 bg-gray-500 bg-opacity-50 top-0 bottom-0 left-0 right-0 flex items-center justify-center"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="relative z-51 bg-black"
+      >
+        <div
+          onClick={modalHandler}
+          className="absolute right-3 top-1 border p-1"
+        >
+          CLOSE
+        </div>
+        <Image width={500} height={500} src="/ruless.png" alt="How to play" />
+      </div>
     </div>
-    </div>
-  )
+  );
 }
--- components/TypoError.tsx
+++ components/TypoError.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-type props ={
-    currentGuess: string
-}
+type props = {
+  currentGuess: string;
+};
 
-export default function TypoError({currentGuess}:props) {
+export default function TypoError({ currentGuess }: props) {
   return (
-    <div className='fixed top-1/4 text-white px-4 py-2 bg-red-800 rounded-md mx-auto'> '{currentGuess.toUpperCase()}' is NOT in a word list.</div>
-  )
+    <div className="fixed top-1/4 text-white px-4 py-2 bg-red-800 rounded-md mx-auto">
+      {' '}
+      '{currentGuess.toUpperCase()}' is NOT in a word list.
+    </div>
+  );
 }
